da8ee384ba1de4a682b8c78ff8d1396839e398d3
Prasanna Kumar Jena <45488940+classicprasana@users.noreply.github.com>
2018-12-02 21:09:13 +0530

    method added by developer -2

--- src/main/java/com/java/techie/git_demo/App.java
+++ src/main/java/com/java/techie/git_demo/App.java
@@ -4,12 +4,15 @@ public class App
 {
 	public void addData(String data)
 	{
-		
 		if(data.equals(Constant.DATA_TYPE))
 				{
 			       System.out.println("conditioncheck::>>");
 				}	
-	}
+	}	
+   public void addMethod()
+   {
+	   System.out.println("adding a new methodin remote repo");
+   }
     public static void main( String[] args )
     {
     	 App ap=new App();

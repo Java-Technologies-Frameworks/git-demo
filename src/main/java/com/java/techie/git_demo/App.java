d69e462ca78dd0bb56421857a055a58dd885421a
Prasanna Kumar Jena <classicprasana@gmail.com>
2018-12-02 10:56:41 +0530

    added git-demo project to local repository

---
+++ src/main/java/com/java/techie/git_demo/App.java
@@ -0,0 +1,19 @@
+package com.java.techie.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void addData(String data)
+	{
+		System.out.println("inside addData method of String type:");
+	}
+    public static void main( String[] args )
+    {
+    	 App ap=new App();
+    	 ap.addData("prasanna");
+        System.out.println( "Hello World!" );
+    }
+}

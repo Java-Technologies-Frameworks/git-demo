f5340cb35b5b81849781c38febf604b78446f37b
Prasanna Kumar Jena <classicprasana@gmail.com>
2018-12-02 19:32:15 +0530

    added constant tolocal repo

--- src/main/java/com/java/techie/git_demo/App.java
+++ src/main/java/com/java/techie/git_demo/App.java
@@ -1,19 +1,14 @@
 package com.java.techie.git_demo;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
 	public void addData(String data)
 	{
-		System.out.println("inside addData method of String type:");
-		if(data.equals("java techie prasanna"))
+		
+		if(data.equals(Constant.DATA_TYPE))
 				{
 			       System.out.println("conditioncheck::>>");
-				}
-				
+				}	
 	}
     public static void main( String[] args )
     {
---
+++ src/main/java/com/java/techie/git_demo/Constant.java
@@ -0,0 +1,6 @@
+package com.java.techie.git_demo;
+
+public class Constant {
+
+  public static final String DATA_TYPE ="java techie prasanna";
+}

419ef720a3f5e29cd347477fc82d2e4ddf0e55bb
Prasanna Kumar Jena <classicprasana@gmail.com>
2019-06-25 22:38:06 +0530

    branch modified

---
+++ src/main/java/com/java/techie/Phase2class.java
@@ -0,0 +1,17 @@
+package com.java.techie;
+
+public class Phase2class {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("THis is for testinga branch newly created ");
+	}
+	
+	
+	public void addNewMethod()
+	{
+		System.out.println("newmethod added");
+		
+	}
+
+}
